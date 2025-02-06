b16ce3f0d1699a04805a6ca80605d9fd3fb2f1c8
김용해 <yoho1019@gmail.com>
2025-02-06 11:02:52 +0900

    CategoryBottomView 다른 파일로 분리

---
+++ SomethingTimer/SomethingTimer/View/ShowCategoryView.swift
@@ -0,0 +1,64 @@
+//
+//  ShowCategoryView.swift
+//  SomethingTimer
+//
+//  Created by 김용해 on 2/6/25.
+//
+
+import SwiftUI
+
+struct ShowCategoryView: View {
+    
+    let gridItem: [GridItem] = Array(repeating: GridItem(.flexible()), count: 4) // gridItem 배열
+    var something: SomethingItem// 받아오기
+//    private let detailCategorys
+// Assets 가져오기
+    var body: some View {
+        VStack {
+            Text("카테고리를 선택해 주세요!")
+                .font(.title)
+                .foregroundStyle(.green)
+            Spacer()
+            LazyVGrid(columns: gridItem, spacing: 20) {
+                ForEach(0...11, id: \.self){_ in
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(.white)
+                        .shadow(radius: 8)
+                        .frame(minHeight: 80)
+                        .overlay {
+                            VStack {
+                                Image(systemName: "01.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                Text("제목")
+                                    .font(.callout)
+                                    
+                            }
+                            .padding(5)
+                        }
+                }
+            }
+            .padding()
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Text("저장")
+            })
+        }
+    }
+}
+
+// 22개의 Assets를 가져오기 위한 Model
+struct DetailCategoryModel {
+    let name: String
+    let imageName: String
+}
+
+
+
+#Preview {
+    ShowCategoryView(something:
+                        SomethingItem(title: "Hello, World!!", timeRemaining: 3600, isFavorite: false, categories: [])
+    )
+}
